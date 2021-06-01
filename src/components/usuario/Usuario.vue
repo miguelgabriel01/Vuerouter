f727b5a63a270a76d785ba8a5aae202fcbcf2396
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-06-01 12:36:56 -0300

    Criando metodo para usar os links entre componentes

--- src/components/usuario/Usuario.vue
+++ src/components/usuario/Usuario.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="usuario">
     <h2>Usuarios</h2>
+    <hr>
+    <button sucesso @click="irParaInicio">Voltar</button>
   </div>
 </template>
 
 <script>
 export default {
+  methods:{
+    irParaInicio(){
+      //pode usar assim
+      //this.$router.push('/')
 
+      //ou assim
+      this.$router.push({path:'/'})
+    }
+  }
 }
 </script>
 
