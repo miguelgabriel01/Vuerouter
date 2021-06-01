f3590e5f796053b9d52e598432887704aa0cd1eb
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-06-01 12:22:54 -0300

    navegando com o rout-link e criando menu basico

---
+++ src/components/templates/Menu.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="menu">
+    <router-link to="/">Inicio</router-link>
+    <router-link to="/usuario">Usuarios</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import Usuario from './components/usuario/Usuario.vue'
 Vue.use(Router)
 
 export default new Router({
+  mode:'history',
   routes:[{
     path: '/',//informa que o /(home) vai ser o componente Inicio
     component: Inicio,//informamos o componente
